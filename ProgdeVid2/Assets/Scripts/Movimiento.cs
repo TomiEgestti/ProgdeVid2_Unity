1daa9f46f009e754d75b2cb5d1f1e64e3cadfe74
TomiEgestti <133979109+TomiEgestti@users.noreply.github.com>
2024-09-11 22:54:47 -0300

    Build del tp2
    Sfx, animaciones y particulas incorporadas

---
+++ ProgdeVid2/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // Variables a configurar desde el editor
+    [Header("Configuracion")]
+    [SerializeField] float velocidad = 5f;
+
+    // Variables de uso interno en el script
+    private float moverHorizontal;
+    private Vector2 direccion;
+
+    // Variable para referenciar otro componente del objeto
+    private Rigidbody2D miRigidbody2D;
+    private Animator miAnimator;
+    private SpriteRenderer miSprite;
+
+    // Codigo ejecutado cuando el objeto se activa en el nivel
+    private void OnEnable()
+    {
+        miRigidbody2D = GetComponent<Rigidbody2D>();
+        miAnimator = GetComponent<Animator>(); 
+        miSprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Codigo ejecutado en cada frame del juego (Intervalo variable)
+    private void Update()
+    {
+        moverHorizontal = Input.GetAxis("Horizontal");
+        direccion = new Vector2(moverHorizontal, 0f);
+
+        int velocidadX = (int)miRigidbody2D.velocity.x;
+        miSprite.flipX = velocidadX > 0f;
+        miAnimator.SetInteger("vel", velocidadX);
+        miAnimator.SetBool("EnAire", miRigidbody2D.velocity.y != 0f);
+    }
+    private void FixedUpdate()
+    {
+        miRigidbody2D.AddForce(direccion * velocidad);
+    }
+}
---
+++ ProgdeVid2/Assets/Scripts/Saltar.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saltar : MonoBehaviour
+{
+
+    // Variables a configurar desde el editor
+    [Header("Configuracion")]
+    [SerializeField] private float fuerzaSalto = 5f;
+    [SerializeField] private AudioClip saltoSFX;
+  
+    // Variables de uso interno en el script
+    private bool puedoSaltar = true;
+    private bool saltando = false;
+
+    // Variable para referenciar otro componente del objeto
+    private Rigidbody2D miRigidbody2D;
+    private AudioSource miAudioSource;
+
+    // Codigo ejecutado cuando el objeto se activa en el nivel
+    private void OnEnable()
+    {
+        miRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Codigo ejecutado en cada frame del juego (Intervalo variable)
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && puedoSaltar)
+        {
+            puedoSaltar = false;
+
+            if (miAudioSource.isPlaying) { return; }
+            miAudioSource.PlayOneShot(saltoSFX);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!puedoSaltar && !saltando)
+        {
+            miRigidbody2D.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+            saltando = true;
+        }
+    }
+
+    // Codigo ejecutado cuando el jugador colisiona con otro objeto
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        puedoSaltar = true;
+        saltando = false;
+    }
+
+}
